d82bc9bed81fc5f4e1b77d0b1090687f894fedc3
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-11-25 14:12:33 +0700

    chore: illustrate division in example

--- example/main.go
+++ example/main.go
@@ -6,59 +6,24 @@ import (
 	"github.com/hadihammurabi/go-result/result"
 )
 
+func Division(a, b int) result.Option[int] {
+	if b == 0 {
+		return result.None[int]()
+	}
+
+	return result.Some(a / b)
+}
+
+func TryDivision(a, b int) {
+	try := Division(a, b)
+	if !try.Ok() {
+		fmt.Printf("%d / %d = failed\n", a, b)
+	} else {
+		fmt.Printf("%d / %d = %d\n", a, b, try.Get())
+	}
+}
+
 func main() {
-	BasicSome()
-	BasicNone()
-	EqualsSome()
-	EqualsNone()
-
-	GlobalUtils()
-}
-
-func BasicSome() {
-	fmt.Println("BasicSome")
-	res := result.Some(10)
-	fmt.Println(res.Ok())
-	fmt.Println(res.OkOr(0))
-	fmt.Println(res)
-
-	fmt.Println()
-}
-
-func BasicNone() {
-	fmt.Println("BasicNone")
-	res := result.None[int]()
-	fmt.Println(res.Ok())
-	fmt.Println(res.OkOr(0))
-	fmt.Println(res)
-
-	fmt.Println()
-}
-
-func EqualsSome() {
-	fmt.Println("EqualsSome")
-	res1 := result.Some(10)
-	res2 := result.Some(10)
-	fmt.Println(res2.Equals(res1))
-
-	fmt.Println()
-}
-
-func EqualsNone() {
-	fmt.Println("EqualsNone")
-	res1 := result.None[int]()
-	res2 := result.Some(1)
-	fmt.Println(res2.Equals(res1))
-
-	fmt.Println()
-}
-
-func GlobalUtils() {
-	fmt.Println("GlobalUtils")
-	fmt.Println(result.IsFalsy(false))
-	fmt.Println(result.IsTruthy("true"))
-	fmt.Println(result.ValueOr("", "default"))
-	fmt.Println(result.ValueOrElse("", func() string { fmt.Println("aduhhhh kosong\t"); return "" }))
-
-	fmt.Println()
+	TryDivision(2, 0)
+	TryDivision(8, 2)
 }
